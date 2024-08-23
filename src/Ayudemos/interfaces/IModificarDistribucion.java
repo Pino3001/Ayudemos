857b7f5c6dbc548f2eefb78b0c9a322cb23b71f6
joaco-sosa <joacososasilva@gmail.com>
2024-08-22 21:47:55 -0300

    Correccion interfaz Modificar Distribucion

--- src/Ayudemos/interfaces/IModificarDistribucion.java
+++ src/Ayudemos/interfaces/IModificarDistribucion.java
@@ -1,13 +1,15 @@
 //    Cada distribución incluye una fecha de preparación y una fecha de entrega, además de un
-//    Universidad de la República | Dirección General de Educación Técnico Profesional
 //    estado que puede ser "PENDIENTE", "EN CAMINO" o "ENTREGADO". Una distribución puede
 //    involucrar una o más donaciones, y está destinada a un beneficiario específico.
 
 package Ayudemos.interfaces;
 
+import Ayudemos.datatypes.DTDonacion;
 import Ayudemos.objects.Beneficiario;
 import Ayudemos.objects.Donacion;
+import Ayudemos.datatypes.DTDistribucion;
 import Ayudemos.types.DTFechaHora;
+import Ayudemos.types.DtBeneficiario;
 import Ayudemos.types.EstadoDistribucion;
 
 import java.util.List;
@@ -15,7 +17,12 @@ import java.util.List;
 
 public interface IModificarDistribucion {
 
-    // Crea una nueva distribución y llama al manejador de las distribuciones para agregarla.
-    public void modificarDistribucion(Beneficiario beneficiario, List<Donacion> donaciones, DTFechaHora fechaPreparacion, DTFechaHora fechaEntrega, EstadoDistribucion estado);
-    
+    void modificarDistribucion(DTDistribucion dtDistribucion);
+
+    // Retornar lista de beneficiarios para cargar el combobox.
+    List<DtBeneficiario> obtenerListaDtBeneficiarios();
+
+    // Retornar lista de donaciones para cargar el combobox.
+    List<DTDonacion> obtenerListaDtDonaciones();
+
 }
