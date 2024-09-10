8921e2074bada7da21b1df881d2df4ac1ad6ea2e
Joaquín <joaquingomezleites@gmail.com>
2024-09-09 22:24:06 -0300

    progreso en la gui de reporte de zonas con mayor distribucion

--- src/main/java/gui/ZonasMayorDistribucionGUI.java
+++ src/main/java/gui/ZonasMayorDistribucionGUI.java
@@ -1,4 +1,11 @@
 package gui;
 
+import javax.swing.*;
+
 public class ZonasMayorDistribucionGUI {
+    private JPanel background;
+    private JTextField textFechaPrepara;
+    private JButton buttonCalendarioPrepara;
+    private JList listaReporte;
+    private JButton buttonAceptarDonacion;
 }
